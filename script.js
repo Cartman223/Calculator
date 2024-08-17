01f4118037ae2115c9392f69b239dd47f5b405c9
Cartman223 <jpirmao37@gmail.com>
2024-08-16 21:42:09 -0300

    create primitive HTML calculator interface for testing purposes

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(operand1, operand2) {
+    return operand1 + operand2
+};
+
+function sub(operand1, operand2) {
+    return operand1 - operand2
+};
+
+function times(operand1, operand2) {
+    return operand1 * operand2
+};
+
+function divide(operand1, operand2) {
+    return operand1 / operand2
+};
+
+function operate(operator, operand1, operand2) {
+    return operator(operand1, operand2)
+};
